f508aba9f3e25cfee9a53d30ef77f320f0083c2b
Kirtan Dudhatra <kirtandudhatra@Kirtans-MacBook-Air.local>
2018-10-16 12:57:18 +0530

    complain feedback added

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var app = express();
 var logindata = require('./controllers/LoginController');
 var sensordata = require('./controllers/SensorDataController');
 
+var complain = require('./controllers/ComplainController');
+var feedback = require('./controllers/FeedbackController');
+
 app.get('/send',function(req,res){
 	
 	sensordata.insertData(req,res);
-	
+	 
 });
 
 
@@ -18,6 +21,16 @@ app.get('/fetch', function(req,res){
 app.get('/login', function(req,res){
 
 	logindata.login(req,res);
+});
+
+app.get('/complain', function(req,res){
+
+	complain.insertData(req,res);
+});
+
+app.get('/feedback', function(req,res){
+
+	feedback.insertData(req,res);
 });
 
 app.listen(3000);
